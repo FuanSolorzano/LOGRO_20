76408d0a88b429f1fe16a216f0556c3398cda328
Juan Solorzano <juanalcachofas6425@gmail.com>
2023-07-20 12:24:42 -0500

    logro_20

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', __('Dashboard'))
+@section('content')
+<div class="container-fluid">
+<div class="row justify-content-center">
+	
+</div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title', __('Welcome'))
+@section('content')
+<div class="container-fluid">
+<div class="row justify-content-center">
+
+</div>
+</div>
+@endsection
